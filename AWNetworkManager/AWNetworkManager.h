9c7ca24b5e0f1619bb626fa792c30e01aebfce9b
TANA <iamtana90@gmail.com>
2020-02-01 13:37:35 +0200

    Initial Commit

---
+++ AWNetworkManager/AWNetworkManager.h
@@ -0,0 +1,19 @@
+//
+//  AWNetworkManager.h
+//  AWNetworkManager
+//
+//  Created by Tudor Ana on 01/02/2020.
+//  Copyright © 2020 Tudor Ana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AWNetworkManager.
+FOUNDATION_EXPORT double AWNetworkManagerVersionNumber;
+
+//! Project version string for AWNetworkManager.
+FOUNDATION_EXPORT const unsigned char AWNetworkManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AWNetworkManager/PublicHeader.h>
+
+
---
+++ AWNetworkManagerTests/AWNetworkManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  AWNetworkManagerTests.swift
+//  AWNetworkManagerTests
+//
+//  Created by Tudor Ana on 01/02/2020.
+//  Copyright © 2020 Tudor Ana. All rights reserved.
+//
+
+import XCTest
+@testable import AWNetworkManager
+
+class AWNetworkManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
