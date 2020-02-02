d65da1575146df82a260db33769bbcbaf512e3a1
TANA <iamtana90@gmail.com>
2020-02-02 12:13:02 +0200

    Fix

--- AWNetworkManager/AWNetworkManager.swift
+++ AWNetworkManager/AWNetworkManager.swift
@@ -10,34 +10,7 @@ import Foundation
 
 public class NetworkManager {
     
-    static func request(with url: String,
-                        method: String = "GET",
-                        headers: [String:String] = [:],
-                        _ completion: @escaping (Data?) -> ()) {
-        
-        let manager = NetworkManager()
-        
-        if let url = URL(string: url) {
-            let request = URLRequest(url: url)
-            manager.begin(request) { (data) in
-                
-                completion(data)
-            }
-        }
-    }
-    
-    static func stopAll() {
-        URLSession.shared.getAllTasks { (tasks) in
-            tasks.forEach({ (task) in
-                task.cancel()
-            })
-        }
-    }
-}
-
-extension NetworkManager {
-    
-    private func begin(_ request: URLRequest,
+    static func begin(_ request: URLRequest,
                       with statusCode: ((_ code: Int) -> Void)? = nil,
                       and response: @escaping (_ response: Data?) -> Void) {
         
@@ -50,8 +23,8 @@ extension NetworkManager {
             
             guard let data = data, error == nil else {
                 if (error?.domain == "NSPOSIXErrorDomain" || error?.domain == "NSURLErrorDomain") {
-                    DispatchQueue.global().asyncAfter(deadline: .now() + 2) { [weak self] in
-                        self?.begin(request, with: statusCode, and: response)
+                    DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
+                        Self.begin(request, with: statusCode, and: response)
                     }
                 }
                 return
@@ -60,7 +33,16 @@ extension NetworkManager {
             response(data)
         }.resume()
     }
+    
+    static func stopAll() {
+        URLSession.shared.getAllTasks { (tasks) in
+            tasks.forEach({ (task) in
+                task.cancel()
+            })
+        }
+    }
 }
+
 
 // MARK: Error
 extension Error {
