4b23c9eefe56d2944fab2e98099c02230c2c615a
TANA <iamtana90@gmail.com>
2020-02-02 11:22:17 +0200

    Tests

--- AWNetworkManager/AWNetworkManager.swift
+++ AWNetworkManager/AWNetworkManager.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+
+public class NetworkManager {
+    
+    static func request(with url: String,
+                        method: String = "GET",
+                        headers: [String:String] = [:],
+                        _ completion: @escaping (Data?) -> ()) {
+        
+        let manager = NetworkManager()
+        
+        if let url = URL(string: url) {
+            let request = URLRequest(url: url)
+            manager.begin(request) { (data) in
+                
+                completion(data)
+            }
+        }
+    }
+    
+    static func stopAll() {
+        URLSession.shared.getAllTasks { (tasks) in
+            tasks.forEach({ (task) in
+                task.cancel()
+            })
+        }
+    }
+}
+
+extension NetworkManager {
+    
+    private func begin(_ request: URLRequest,
+                      with statusCode: ((_ code: Int) -> Void)? = nil,
+                      and response: @escaping (_ response: Data?) -> Void) {
+        
+        
+        URLSession.shared.dataTask(with: request) { (data, httpResponse, error) in
+            
+            #if DEBUG
+            print("Response url : \(String(describing: request.url)) : \(String(describing: String(data: data ?? Data(), encoding: .utf8)))")
+            #endif
+            
+            guard let data = data, error == nil else {
+                if (error?.domain == "NSPOSIXErrorDomain" || error?.domain == "NSURLErrorDomain") {
+                    DispatchQueue.global().asyncAfter(deadline: .now() + 2) { [weak self] in
+                        self?.begin(request, with: statusCode, and: response)
+                    }
+                }
+                return
+            }
+
+            response(data)
+        }.resume()
+    }
+}
+
+// MARK: Error
+extension Error {
+    var code: Int { return (self as NSError).code }
+    var domain: String { return (self as NSError).domain }
+}
--- AWNetworkManagerTests/AWNetworkManagerTests.swift
+++ AWNetworkManagerTests/AWNetworkManagerTests.swift
@@ -20,14 +20,17 @@ class AWNetworkManagerTests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        NetworkManager.request(with: "https://www.google.ro") { (data) in
+            
+        }
     }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            NetworkManager.request(with: "https://www.google.ro") { (data) in
+                
+            }
         }
     }
 
