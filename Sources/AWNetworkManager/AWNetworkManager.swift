375a2f17eecb849696987d3b355c285216b768a5
TANA <iamtana90@gmail.com>
2022-03-01 23:58:36 +0200

    Minor refactoring

--- Sources/AWNetworkManager/AWNetworkManager.swift
+++ Sources/AWNetworkManager/AWNetworkManager.swift
@@ -2,24 +2,17 @@ import Foundation
 
 public class AWNetworkManager {
 
-    public enum Result<Success, Failure: Error> {
-        case success(Success)
-        case failure(Failure)
-    }
-
     static public func begin(_ request: URLRequest,
                              retry: Bool = false,
                              verbose: Bool = false,
                              _ result: @escaping (Result<Data, Error>) -> Void) {
-
+        
         URLSession.shared.dataTask(with: request) { (data, status, error) in
-
+#if DEBUG
             if verbose {
-                #if DEBUG
-                print("Response url: \(String(describing: request.url)) : \(String(describing: String(data: data ?? Data(), encoding: .utf8))) : \(status)")
-                #endif
+                print("Response url: \(String(describing: request.url)) : \(String(describing: String(data: data ?? Data(), encoding: .utf8)))")
             }
-
+#endif
             guard let data = data, error == nil else {
                 
                 if (error?.domain == "NSPOSIXErrorDomain" || error?.domain == "NSURLErrorDomain") && retry {
@@ -31,11 +24,12 @@ public class AWNetworkManager {
                 result(.failure(error!))
                 return
             }
-
+            
             result(.success(data))
+            
         }.resume()
     }
-
+    
     @available(macOS 10.11, *)
     @available(iOS 9.0, *)
     static public func stopAll() {
@@ -46,7 +40,6 @@ public class AWNetworkManager {
         }
     }
 }
-
 
 // MARK: Error
 extension Error {
